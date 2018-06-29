f82716595317f4c74f661ec2bbd580d3126f9fea
Emily Morgado <emorgado@sfomlp-1jchv2h.ad.businesswire.com>
2018-06-29 09:30:24 -0700

    Reducer modifications

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -14,12 +14,7 @@ const reducer = (state = initialState, action) => {
   switch(action.type) {
     case actionTypes.STORE_RESULT:
       return updateObject(state, {results: state.results.concat({id: new Date(), value: action.result * 2})});
-        // Notice, we are manipulating state before sending it to the action creator
-        // We could manipulate it in the action creator, but here is better
-        // you can use concat with arrays to push results immutabley.
-        // It creates a clone and adds your result. This keeps state predictable.
-    case actionTypes.DELETE_RESULT:
-      return deleteResult(state, action);
+    case actionTypes.DELETE_RESULT: return deleteResult(state, action);
 
   }
   return state;
